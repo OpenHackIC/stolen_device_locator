6c8293e59d492b8be033c363efa0eefec9790302
Ben Oakes <github@benjaminoakes.com>
2014-01-22 10:01:24 -0600

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-tshark -i wlan0mon -T fields -e 'wlan.ra' -e 'wlan.sa' -e 'wlan.ta' -e 'wlan.da' | ruby1.9.1 thing.rb
+tshark -i wlan0mon -T fields -e 'wlan.ra' -e 'wlan.sa' -e 'wlan.ta' -e 'wlan.da' | ruby1.9.1 run.rb
